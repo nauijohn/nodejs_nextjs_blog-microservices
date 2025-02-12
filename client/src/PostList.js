649ee29a7d92eeee42c0196b9b21df105b4407b2
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-02-13 06:55:06 +0800

    Section 2: Displaying Comments

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 function PostList() {
   const [posts, setPosts] = useState({});
@@ -22,10 +24,13 @@ function PostList() {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
   });
+
   return (
     <div className="d-flex flex-row flex-wrap justify-content-between">
       {renderedPosts}
