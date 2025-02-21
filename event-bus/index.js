eba00e8af3ecb30dd8dfeb58e8717d2c41897534
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-02-21 23:48:59 +0800

    Section 2: 33. Emitting Comment Creation Events

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,12 +6,18 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', async (req, res) => {
-  const event = req.body;
+  try {
+    console.log('Received Event', req.body.type);
+    const event = req.body;
 
-  axios.post('http://localhost:4000/events', event);
-  axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event);
-
+    await Promise.allSettled([
+      axios.post('http://localhost:4000/events', event),
+      axios.post('http://localhost:4001/events', event),
+      axios.post('http://localhost:4002/events', event),
+    ]);
+  } catch (error) {
+    console.error(error);
+  }
   res.send({ status: 'OK' });
 });
 
