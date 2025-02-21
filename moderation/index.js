83ffaa239c0ac7a8a001accd54efd829e0f2a6c6
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-02-22 05:39:11 +0800

    Section 2: 45. Handling Moderation

--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,25 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', async (req, res) => {});
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        content: data.content,
+        postId: data.postId,
+        status,
+      },
+    });
+  }
+
+  res.send({});
+});
 
 app.listen(4003, async () => {
   console.log('Moderation service listening on port 4003...');
