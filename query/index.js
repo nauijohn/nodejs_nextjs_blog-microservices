95263611c6676d6c1f072dea8c5573a2d0989c47
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-02-22 23:56:27 +0800

    Section 3: 86. Adding Query, Moderation and Comments

--- query/index.js
+++ query/index.js
@@ -11,27 +11,35 @@ app.use(cors());
 const posts = {};
 
 const handleEvent = (type, data) => {
-  if (type === 'PostCreated') {
-    const { id, title } = data;
+  switch (type) {
+    case 'PostCreated':
+      const { id, title } = data;
+      posts[id] = { id, title, comments: [] };
+      break;
 
-    posts[id] = { id, title, comments: [] };
-  }
+    case 'CommentCreated':
+      const { id: commentId, content, postId, status } = data;
+      const post = posts[postId];
+      post.comments.push({ id: commentId, content, status });
+      break;
 
-  if (type === 'CommentCreated') {
-    const { id, content, postId, status } = data;
+    case 'CommentUpdated':
+      const {
+        id: updatedCommentId,
+        content: updatedContent,
+        postId: updatedPostId,
+        status: updatedStatus,
+      } = data;
+      const updatedPost = posts[updatedPostId];
+      const comment = updatedPost.comments.find(
+        (comment) => comment.id === updatedCommentId,
+      );
+      comment.status = updatedStatus;
+      comment.content = updatedContent;
+      break;
 
-    const post = posts[postId];
-    post.comments.push({ id, content, status });
-  }
-
-  if (type === 'CommentUpdated') {
-    const { id, content, postId, status } = data;
-
-    const post = posts[postId];
-    const comment = post.comments.find((comment) => comment.id === id);
-
-    comment.status = status;
-    comment.content = content;
+    default:
+      break;
   }
 };
 
@@ -50,7 +58,7 @@ app.post('/events', (req, res) => {
 app.listen(4002, async () => {
   console.log('Query service listening on 4002...');
 
-  const res = await axios.get('http://localhost:4005/events');
+  const res = await axios.get('http://event-bus-srv:4005/events');
 
   for (const event of res.data) {
     console.log('Processing event:', event.type);
